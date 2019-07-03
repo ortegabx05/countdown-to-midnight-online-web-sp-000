3465c050feea0a0e965aa75e07a5c9a670831913
jessi ortega <ortegabx05@gmail.com>
2019-07-03 13:47:27 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
 
-def countdown
+
+def countdown(x)
+  x=10
+  while x > 0 
+   puts "#{x} SECOND(S)!"
+   x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num) 
+ one_sec = sleep 5
+  while num > 0
+  puts "#{num}" 
+     num -= one_sec
+  end 
 end
