e7196c140c4e2c4191b52a13c5fcbe5df298ff3e
jessi ortega <ortegabx05@gmail.com>
2019-07-29 18:05:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,17 @@
 
 
 def countdown(x)
+<<<<<<< HEAD
+=======
+  countdown_with_sleep(5.seconds)
+>>>>>>> b9952a4ff2769d23bddf0cc71fd28f2786b81b88
   x=10
   while x > 0 
    puts "#{x} SECOND(S)!"
    x -= 1
   end
   return "HAPPY NEW YEAR!"
+<<<<<<< HEAD
 end
 
 def countdown_with_sleep(num) 
@@ -15,4 +20,6 @@ def countdown_with_sleep(num)
   puts "#{num}" 
      num -= one_sec
   end 
+=======
+>>>>>>> b9952a4ff2769d23bddf0cc71fd28f2786b81b88
 end
